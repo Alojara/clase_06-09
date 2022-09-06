14bb2315622ea22a70d16394bfc41f3f191cb3a3
Alonso <alo.jara@duocuc.cl>
2022-09-06 19:03:21 -0300

    clase 06-09

---
+++ src/app/componentes/pokemon/pokemon.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { HistoriaImagen } from './../../modelos/historia-imagen' //../ es para subir un nivel en las carpetas
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss']
+})
+export class PokemonComponent{
+  @Input() public pokemon!: HistoriaImagen;
+
+  @Output() public idPokemon = new EventEmitter<number>();
+
+  public emitirId(): void{
+    this.idPokemon.emit(this.pokemon.id);
+  }
+}
